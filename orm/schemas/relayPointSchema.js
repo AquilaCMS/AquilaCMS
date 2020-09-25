872eca1b3c6687d468456e996f1007bd2cc466e6
Gérard Le Cloerec <gerard.le.cloerec@gmail.com>
2020-09-25 09:17:26 +0200

    docs: - paymentMethods - pictos - productVirtual - relayPoint - sequences - setOptions - shortcodes - specialCond - staticsPreview - statics - statsHistory - statsToday - suppliers - themeConfig - trademarks

--- orm/schemas/relayPointSchema.js
+++ orm/schemas/relayPointSchema.js
@@ -1,5 +1,21 @@
 const mongoose         = require('mongoose');
 const Schema           = mongoose.Schema;
+
+/**
+ * @typedef {object} RelayPointSchema
+ * @property {RelayPointSchemaAddress} address
+ */
+
+/**
+ * @typedef {object} RelayPointSchemaAddress
+ * @property {string} name
+ * @property {string} line1
+ * @property {string} line2
+ * @property {string} zipcode
+ * @property {string} city
+ * @property {string} isoCountryCode
+ * @property {string} country
+ */
 const RelayPointSchema = new Schema({
     address : {
         name           : String,
--- orm/schemas/themeConfigSchema.js
+++ orm/schemas/themeConfigSchema.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
+/**
+ * @typedef {object} ThemeConfigSchema
+ * @property {string} name
+ * @property {object} config
+ */
 const ThemeConfigSchema = new Schema({
     name   : String,
     config : {}
