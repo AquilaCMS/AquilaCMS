5375143d38a1af5629fe4d8cda006d4dc7765a50
jorickBarneaud <72439796+jorickBarneaud@users.noreply.github.com>
2020-11-19 11:52:34 +0100

    [fix] delete presave contact

--- orm/models/contacts.js
+++ orm/models/contacts.js
@@ -1,8 +1,4 @@
 const mongoose         = require('mongoose');
 const {ContactsSchema} = require('../schemas');
 
-ContactsSchema.pre('save', function (next) {
-    next();
-}, {timestamps: true});
-
 module.exports = mongoose.model('contacts', ContactsSchema, 'contacts');
