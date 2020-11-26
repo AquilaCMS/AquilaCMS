39b13fd9645689aa23711ef3156911d2f736c25d
Gérard Le Cloerec <gerard.le.cloerec@gmail.com>
2020-11-26 10:35:28 +0100

    fix: byString rename to objectPropertybyString

--- services/products.js
+++ services/products.js
@@ -496,24 +496,36 @@ const getProductsByCategoryId = async (id, PostBody = {}, lang, isAdmin = false,
     };
 };
 
-const orderByPriceSort = (tProducts, PostBody, param = 'price.priceSort.et') => {
-    const byString = (o, s) => {
-        s       = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-        s       = s.replace(/^\./, '');           // strip a leading dot
-        const a = s.split('.');
-        for (let i = 0, n = a.length; i < n; ++i) {
-            const k = a[i];
-            if (k in o) {
-                o = o[k];
-            } else {
-                return;
-            }
+/**
+ * find object property by string
+ * @example
+ * const obj = {p1: {p2: "test"}}
+ * const value = objectPropertybyString(obj, "p1.p2")
+ * console.log(value) // "test"
+ *
+ * @param {object} o object to find value from s
+ * @param {string} s property to find value from
+ * @return {any} return the value of the property found
+ */
+const objectPropertybyString = (o, s) => {
+    s       = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    s       = s.replace(/^\./, '');           // strip a leading dot
+    const a = s.split('.');
+    for (let i = 0, n = a.length; i < n; ++i) {
+        const k = a[i];
+        if (k in o) {
+            o = o[k];
+        } else {
+            return;
         }
-        return o;
-    };
+    }
+    return o;
+};
+
+const orderByPriceSort = (tProducts, PostBody, param = 'price.priceSort.et') => {
     return tProducts.sort((a, b) => {
-        const ea = byString(a, param);
-        const eb = byString(b, param);
+        const ea = objectPropertybyString(a, param);
+        const eb = objectPropertybyString(b, param);
         if (ea > eb) {
             return PostBody.sort[param] ? 1 : -1;
         }
