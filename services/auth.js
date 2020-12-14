f452043135e5c8e959d1b00ede262c72d9b05a0e
CARRIERE Nicolas <oxid83@gmail.com>
2020-12-14 14:31:14 +0100

    [fix] check isActiveAccount

--- services/auth.js
+++ services/auth.js
@@ -38,7 +38,7 @@ const login = async (req, res, next) => {
         if (req.params.from === 'admin') {
             if (!user.isAdmin) throw NSErrors.Unauthorized;
         }
-        if (!user.isActive) {
+        if (!user.isActiveAccount) {
             throw NSErrors.DesactivateAccount;
         }
 
