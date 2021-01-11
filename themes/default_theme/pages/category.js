70d01b04830d108ad7b59558943a93cc5d808bda
Yos666666 <ngiusti@orange.fr>
2021-01-11 10:56:30 +0100

    #TPD [fix] /category & /product : add hook select-date [fix] cart/success : adjustment for withdrawal

--- themes/default_theme/pages/category.js
+++ themes/default_theme/pages/category.js
@@ -9,10 +9,10 @@ import {
 } from 'aqlrc';
 import ReactPagination from 'react-js-pagination';
 import Layout from 'components/Layout';
+import { listModulePage } from 'lib/utils';
 import { withI18next } from 'lib/withI18n';
 import routes, { Link, Router } from 'routes';
 import CMS from 'components/CMS';
-import { listModulePage } from 'lib/utils';
 import Error from './_error';
 
 /**
@@ -59,6 +59,9 @@ class PageCategory extends NSPageCategory {
 
                         <div className="main">
                             <div className="shell">
+                                {
+                                    listModulePage('select-date')
+                                }
                                 <NSBreadcrumb gNext={{ routes, Link }} />
 
                                 { nsCms_extraText2 && nsCms_extraText2.content !== '' && <div style={{ marginBottom: '20px' }}><CMS content={nsCms_extraText2.content} hide_error="1" /></div> }
