873fb224c0b6d731fd8171c9de03d4cb549d375a
jorickBarneaud <72439796+jorickBarneaud@users.noreply.github.com>
2021-01-26 16:31:11 +0100

    [fix] admin > catalog > product : global filter

--- backoffice/app/product/product.controllers.js
+++ backoffice/app/product/product.controllers.js
@@ -151,11 +151,11 @@ ProductControllers.controller("ProductListCtrl", [
                 return '';
             }
         };
-        $scope.collapse = function () {
-          if(document.getElementById('collapseIcon').className === "fa fa-chevron-down fa-3x"){
-            document.getElementById('collapseIcon').className =  "fa fa-chevron-up fa-3x"
-          }else {document.getElementById('collapseIcon').className = "fa fa-chevron-down fa-3x"}
-        };
+        // $scope.collapse = function () {
+        //   if(document.getElementById('collapseIcon').className === "ico-arrow-down"){
+        //     document.getElementById('collapseIcon').className =  "ico-arrow-up"
+        //   }else {document.getElementById('collapseIcon').className = "ico-arrow-down"}
+        // };
 
         $scope.getAttributesClassed = function () {
             AttributesV2.list({PostBody: {filter: {_type: 'products'}, limit: 99}}, function ({datas}) {
