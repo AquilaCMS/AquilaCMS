820718fe90a159fb63bf209d204dedd66a4fd380
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-02-26 17:02:51 +0100

    [fix] password recover

--- backoffice/assets/js/pages/login.js
+++ backoffice/assets/js/pages/login.js
@@ -4,10 +4,26 @@ $(document).ready(function () {
         checkboxClass : "icheckbox_square-blue"
     });
 
-    $("form").submit(async function (e) {
-        e.preventDefault();
+    function hideError(){
         $("#error_msg_bad_login").hide();
         $("#error_msg_disable_account").hide();
+        $("#api-error").hide();
+        $("#resetPass").hide();
+        $("#resetedPass").hide();
+    }
+
+    var url = window.location.href;
+    var urlObject = new URL(url);
+    if(urlObject.searchParams.get("token")){
+        document.getElementById('form1').style.display = "none";
+        document.getElementById('form2').style.display = "none";
+        document.getElementById('form3').style.display = "block";
+        document.getElementById('passwordForgot').style.display = "none";
+    }
+
+    $("#form1").submit(async function (e) {
+        e.preventDefault();
+        hideError();
         try {
             const response = await $.post(window.location.origin + "/api/v2/auth/login/admin", {
                 username: $("#field-email", this).val(),
@@ -23,4 +39,58 @@ $(document).ready(function () {
             }
         }
     });
+
+    $("#form2").submit(async function (e) {
+        e.preventDefault();
+        hideError();
+        try {
+            const response = await $.post(window.location.origin + "/api/v2/user/resetpassword", {
+                email: $("#field-email2", this).val()
+            });
+            document.getElementById('resetPass').innerHTML = "<strong>Email envoyé à " + $("#field-email2", this).val() + "</strong>";
+            document.getElementById('resetPass').style.display = "block";
+        } catch (err) {
+            $("#api-error").show();
+
+        }
+    });
+
+    document.getElementById('passwordForgot').onclick = function(){
+        let form1 = document.getElementById('form1');
+        let form2 = document.getElementById('form2');
+        if(form1.style.display == "none"){
+            form2.style.display = "none";
+            form1.style.display = "block";
+            this.innerHTML = "Forgot your password? Reset your password";
+        }else{
+            form2.style.display = "block";
+            form1.style.display = "none";
+            this.innerHTML = "Remember your password? Connect now !";
+        }
+    }
+
+    $("#form3").submit(async function (e) {
+        e.preventDefault();
+        hideError();
+        const passwordValidator = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$");
+        if(passwordValidator.test($("#new-password1", this).val())){
+            if($("#new-password1", this).val() == $("#new-password2", this).val()){
+                try {
+                    const response = await $.post(window.location.origin + "/api/v2/user/resetpassword", {
+                        password: $("#new-password1", this).val() ,
+                        token: urlObject.searchParams.get("token")
+                    });
+                    window.location.href = window.location.origin + window.location.pathname;
+                } catch (err) {
+                    $("#api-error").show();
+                }
+            }else{
+                
+            }
+        }else{
+            $("#resetedPass").show();
+        }
+    });
+
 });
+
