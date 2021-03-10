ad7dd433ef28f88df593744475a30468acb5ab9d
Gérard Le Cloerec <gerard.le.cloerec@gmail.com>
2021-03-10 15:48:27 +0100

    style: add module services to the backoffice

--- backoffice/app/modules/modules.module.js
+++ backoffice/app/modules/modules.module.js
@@ -1,8 +1,9 @@
 var currModule = {name: 'modules'};
 
-angular.module(`aq.${currModule.name}`, [`aq.${currModule.name}.routes`, // 'aq.' + currModule.name + '.services',
-    `aq.${currModule.name}.controllers`
-    // 'aq.' + currModule.name + '.directives'
+angular.module(`aq.${currModule.name}`, [
+    "aq." + currModule.name + ".routes",
+    "aq." + currModule.name + ".services",
+    "aq." + currModule.name + ".controllers"
 ]);
 
 angular.module(`aq.${currModule.name}`).constant('CstModules', {
