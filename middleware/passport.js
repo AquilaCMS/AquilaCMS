fd36eefa63ad4726c13093a41a73a0b7852bbe86
devaquila <teamdev@nextsourcia.com>
2021-03-16 10:29:56 +0100

    [Fix] Authenticate control and clear bad cookie

--- middleware/passport.js
+++ middleware/passport.js
@@ -89,9 +89,10 @@ const init = async (pp) => {
  * Authenticate
  */
 const authenticate = (req, res) => new Promise((resolve, reject) => {
+    const _res = res;
     passport.authenticate('jwt', {session: false}, (err, user) => {
         if (err) reject(err);
-        // else if (!user) reject(NSErrors.Unauthorized);
+        else if (!user) _res.clearCookie('jwt'); // reject(NSErrors.Unauthorized);
         resolve(user);
     })(req, res);
 });
