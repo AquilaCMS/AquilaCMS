579235e713764f734e8d5823b931a436965787d2
devaquila <teamdev@nextsourcia.com>
2021-05-28 16:10:48 +0200

    [Fix] Unit test : active

--- test/utils/createStaticPage.js
+++ test/utils/createStaticPage.js
@@ -1,13 +1,14 @@
 const faker     = require('faker');
 const {Statics} = require('../../orm/models');
 
-const createStaticPage = (params = {code: null, content: null, title: null}) => {
-    const {code, content, title} = params;
-    const staticPage             = new Statics();
-    staticPage.type              = 'page';
-    staticPage.group             = null;
-    staticPage.code              = code || faker.lorem.slug();
-    staticPage.translation       = {
+const createStaticPage = (params = {code: null, content: null, title: null, active: false}) => {
+    const {code, content, title, active} = params;
+    const staticPage                     = new Statics();
+    staticPage.type                      = 'page';
+    staticPage.group                     = null;
+    staticPage.code                      = code || faker.lorem.slug();
+    staticPage.active                    = active;
+    staticPage.translation               = {
         fr : {
             variables : [],
             html      : '',
