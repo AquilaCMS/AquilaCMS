74de26636efe1a80e2174010df70393fe3370fc9
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2021-07-30 09:28:24 +0200

    [fix] change checkAccess

--- backoffice/app/contact/contact.routes.js
+++ backoffice/app/contact/contact.routes.js
@@ -1,24 +1,22 @@
 var ContactRoutes = angular.module('aq.contact.routes', ['ngRoute']);
 
 ContactRoutes.config(['$routeProvider',
-
     function ($routeProvider) {
-
         $routeProvider
-        .when('/contacts', {
-            templateUrl: 'app/contact/views/contact-list.html',
-            controller: 'ContactListCtrl',
-            resolve: {
-                loggedin: checkLoggedin,
-                checkAccess: 'contacts'
-            }
-        }).when('/contact/:id', {
-            templateUrl: 'app/contact/views/contact-detail.html',
-            controller: 'ContactDetailsCtrl',
-            resolve: {
-                loggedin: checkLoggedin,
-                checkAccess: 'contacts'
-            }
-        });
+            .when('/contacts', {
+                templateUrl: 'app/contact/views/contact-list.html',
+                controller: 'ContactListCtrl',
+                resolve: {
+                    loggedin: checkLoggedin,
+                    checkAccess: checkAccess('contacts')
+                }
+            }).when('/contact/:id', {
+                templateUrl: 'app/contact/views/contact-detail.html',
+                controller: 'ContactDetailsCtrl',
+                resolve: {
+                    loggedin: checkLoggedin,
+                    checkAccess: checkAccess('contacts')
+                }
+            });
 
     }]);
