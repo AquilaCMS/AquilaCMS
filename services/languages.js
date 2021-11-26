256bc29bcb3d2bfcc6bbf090f75a3fefe368279c
CARRIERE Nicolas <oxid83@gmail.com>
2021-11-26 10:45:33 +0100

    Auto stash before merge of "need-restart-rebuild" and "origin/need-restart-rebuild"

--- services/languages.js
+++ services/languages.js
@@ -133,6 +133,7 @@ const createDynamicLangFile = async (selectedTheme = global.envConfig.environmen
         }
 
         await fs.writeFile(linkToFile, contentFile);
+        console.og('5555');
     } catch (e) {
         console.log(e);
         throw 'Error writing file "dynamic_langs.js"';
