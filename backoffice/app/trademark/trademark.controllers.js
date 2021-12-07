2942af72472ecf917da9a7828a52381a7c560254
CARRIERE Nicolas <oxid83@gmail.com>
2021-12-07 15:51:35 +0100

    [fix] trademark name already exists at creation

--- backoffice/app/trademark/trademark.controllers.js
+++ backoffice/app/trademark/trademark.controllers.js
@@ -136,7 +136,7 @@ TrademarkControllers.controller('TrademarkNewCtrl', ['$scope', '$location', 'toa
             }, function(error){
                 if(error.data){
                     if(error.data.message && error.data.message != ""){
-                        toastService.toast("danger",  error.data.message);
+                        toastService.toast("danger",  $translate.instant("trademark.detail.errorName"));
                     }
                 }else if(error && error.code != ""){
                     toastService.toast("danger", error.code);
