0ce5eacf8c74c3b193f4d4d02748ff258c140f85
devaquila <71753125+devaquila@users.noreply.github.com>
2022-02-15 08:45:13 +0100

    [fix] Wrong roduct variant display type into enum
    (replicate from dev)

--- orm/schemas/productSimpleSchema.js
+++ orm/schemas/productSimpleSchema.js
@@ -27,7 +27,7 @@ const ProductSimpleSchema = new Schema({
     },
     variants : [{
         code        : {type: String},
-        type        : {type: String, enum: ['list', 'radio', 'images', 'list2']},
+        type        : {type: String, enum: ['list', 'radio', 'image', 'list2']},
         sort        : {type: Number},
         id          : {type: ObjectId, ref: 'attributes', index: true},
         translation : {
