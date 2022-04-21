4256464759f4b283af915516591eb5e34a4f05a4
BOILLOT DEVALLIERE Vincent <63784031+JustSeenn@users.noreply.github.com>
2022-04-21 17:23:11 +0200

    Correctif bug

--- orm/plugins/generateID.js
+++ orm/plugins/generateID.js
@@ -2,12 +2,12 @@ const utilsDatabase = require('../../utils/database');
 const mongoose = require('mongoose')
 
 const generate = () => {
-    return mongoose.Types.ObjectId([...Array(24)].map(() => Math.floor(Math.random() * 16).toString(16)).join(''));
+    return mongoose.Types.ObjectId("ff" + [...Array(22)].map(() => Math.floor(Math.random() * 16).toString(16)).join(''));
 }
 
 module.exports = function generateID(schema){
     schema.pre('save', async function (next) {
-        this._id = generate()
+        if(this._id.id[0] != 255) this._id = generate()
         await utilsDatabase.preUpdates(this, next, schema);
     });
 }
