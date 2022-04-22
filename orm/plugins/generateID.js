5eeb97c37be73c4eaa5ed9a04b6ec5741138915a
BOILLOT DEVALLIERE Vincent <63784031+JustSeenn@users.noreply.github.com>
2022-04-22 10:49:07 +0200

    Eslint beautify

--- orm/plugins/generateID.js
+++ orm/plugins/generateID.js
@@ -1,13 +1,11 @@
+const mongoose      = require('mongoose');
 const utilsDatabase = require('../../utils/database');
-const mongoose = require('mongoose')
 
-const generate = () => {
-    return mongoose.Types.ObjectId("ff" + [...Array(22)].map(() => Math.floor(Math.random() * 16).toString(16)).join(''));
-}
+const generate = () => mongoose.Types.ObjectId(`ff${[...Array(22)].map(() => Math.floor(Math.random() * 16).toString(16)).join('')}`);
 
-module.exports = function generateID(schema){
+module.exports = function generateID(schema) {
     schema.pre('save', async function (next) {
-        if(this._id.id[0] != 255) this._id = generate()
+        if (this._id.id[0] !== 255) this._id = generate();
         await utilsDatabase.preUpdates(this, next, schema);
     });
-}
+};
