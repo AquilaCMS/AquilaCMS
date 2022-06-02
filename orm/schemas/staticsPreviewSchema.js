05a983168601a9e2c839cf7b548c17e220c70bc6
Nicolas <oxid83@gmail.com>
2022-06-02 14:58:13 +0200

    fix static preview conflict error

--- orm/schemas/staticsPreviewSchema.js
+++ orm/schemas/staticsPreviewSchema.js
@@ -6,28 +6,22 @@
  * Disclaimer : Do not edit or add to this file if you wish to upgrade AQUILA CMS to newer versions in the future.
  */
 
-const mongoose      = require('mongoose');
-const Statics       = require('./staticsSchema');
-const utilsDatabase = require('../../utils/database');
-const Schema        = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
 
 /**
  * @typedef {StaticsSchema} StaticsPreviewSchema
  */
-const StaticsPreviewSchema   = new Schema(Statics);
-StaticsPreviewSchema.statics = Statics.statics;
-
-StaticsPreviewSchema.pre('updateOne', async function (next) {
-    utilsDatabase.preUpdates(this, next, StaticsPreviewSchema);
-});
-
-StaticsPreviewSchema.pre('findOneAndUpdate', async function (next) {
-    utilsDatabase.preUpdates(this, next, StaticsPreviewSchema);
-});
-
-StaticsPreviewSchema.pre('save', async function (next) {
-    const errors = await StaticsPreviewSchema.statics.translationValidation(undefined, this);
-    next(errors.length > 0 ? new Error(errors.join('\n')) : undefined);
+const StaticsPreviewSchema = new Schema({
+    code        : {type: String, required: true, unique: true},
+    type        : {type: String, required: true},
+    active      : {type: Boolean, default: false},
+    group       : {type: String, default: ''},
+    // index        : {type: Boolean, default: true},
+    translation : {}
+}, {
+    timestamps : true,
+    id         : false
 });
 
 module.exports = StaticsPreviewSchema;
