ab8a625d841277d7402d995a2fbe771898a92500
bertrand-ns <bertrand.pihan@nextsourcia.com>
2022-09-21 10:39:06 +0200

    feat: fuse search custom fields

--- services/products.js
+++ services/products.js
@@ -144,13 +144,9 @@ const getProductsByOrderedSearch = async (pattern, filters, lang = global.defaul
     const config         = await Configuration.findOne({}, {'environment.searchSettings': 1}).lean();
     const searchSettings = config?.environment?.searchSettings;
 
-    const selectedFields                = `translation.${lang}.name code translation.${lang}.description1.title translation.${lang}.description1.text translation.${lang}.description2.title translation.${lang}.description2.text`;
-    const allProductsWithSearchCriteria = await Products.find(filters).select(selectedFields).lean();
-
     for (let index = 0; index < searchSettings.keys.length; index++) {
         searchSettings.keys[index].name = searchSettings.keys[index].name.replace('{lang}', lang);
     }
-
     const selectedFieldsArray = [
         {name: `translation.${lang}.name`, weight: 10},
         {name: 'code', weight: 20},
@@ -158,6 +154,12 @@ const getProductsByOrderedSearch = async (pattern, filters, lang = global.defaul
         {name: `translation.${lang}.description1.text`, weight: 2.5},
         {name: `translation.${lang}.description2.title`, weight: 2},
         {name: `translation.${lang}.description2.text`, weight: 1.5}];
+    const keySearch           = (searchSettings.keys !== undefined && searchSettings.keys.length !== 0) ? searchSettings.keys : selectedFieldsArray;
+    const selectedFields      = keySearch.map(function (item) {
+        return item.name;
+    });
+
+    const allProductsWithSearchCriteria = await Products.find(filters).select(selectedFields).lean();
 
     // To adapt the options see the following link https://fusejs.io/concepts/scoring-theory.html#scoring-theory
     const options = {
@@ -169,7 +171,7 @@ const getProductsByOrderedSearch = async (pattern, filters, lang = global.defaul
         useExtendedSearch  : searchSettings.useExtendedSearch !== undefined ? searchSettings.useExtendedSearch : true,
         minMatchCharLength : searchSettings.minMatchCharLength !== undefined ? searchSettings.minMatchCharLength : 2,
         threshold          : searchSettings.threshold !== undefined ? searchSettings.threshold : 0.2, // 0.2 and 0.3 are the recommended values
-        keys               : (searchSettings.keys !== undefined && searchSettings.keys.length !== 0) ? searchSettings.keys : selectedFieldsArray
+        keys               : keySearch
     };
 
     const fuse    = new Fuse(allProductsWithSearchCriteria, options);
