1da2df859914b4f6b4086ac00d7e77c2cb1e480b
Yos666666 <ngiusti@orange.fr>
2022-09-27 11:30:26 +0200

    [feature] Display type available for all variants used

--- backoffice/app/product/product.directives.js
+++ backoffice/app/product/product.directives.js
@@ -294,7 +294,21 @@ ProductDirectives.directive("nsProductDeclinaisons", function () {
 
             $scope.getProductVariantIndex = function (code) {
                 return $scope.product.variants.findIndex(v => v.code === code)
-            }     
+            }
+
+            $scope.updateVariantType = function (value) {
+                if (value === 'list2') {
+                    for (let i = 0; i < $scope.product.variants.length; i++) {
+                        $scope.product.variants[i].type = value;
+                    }
+                } else {
+                    for (let i = 0; i < $scope.product.variants.length; i++) {
+                        if ($scope.product.variants[i].type === 'list2') {
+                            $scope.product.variants[i].type = value;
+                        }
+                    }
+                }
+            }
 
             $scope.getImageUrl = function (variant) {
                 let defaultImage = variant.images.find(img => img.default)
