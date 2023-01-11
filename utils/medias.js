96bbc67f2cc48636cba92a7993cb017d5d97d14f
bertrand-ns <bertrand.pihan@nextsourcia.com>
2023-01-11 15:52:29 +0100

    [fix] cart return default image or empty

--- utils/medias.js
+++ utils/medias.js
@@ -61,10 +61,11 @@ const getProductImageUrl = (product) => {
 };
 
 const getProductImageId = (product) => {
+    const defaultImage = global.envConfig.environment.defaultImage ? 'no-image' : '';
     if (product.selected_variant && product.selected_variant.images) {
-        return product.selected_variant.images.find((img) => img.default) ? product.selected_variant.images.find((img) => img.default)._id : 'no-image';
+        return product.selected_variant.images.find((img) => img.default) ? product.selected_variant.images.find((img) => img.default)._id : defaultImage;
     }
-    return product.images.find((i) => i.default) ? product.images.find((i) => i.default)._id : 'no-image';
+    return product.images.find((i) => i.default) ? product.images.find((i) => i.default)._id : defaultImage;
 };
 
 // Generic file deletion function
