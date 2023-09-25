c3285a86513b61a09f069bfd5ca2d9a3c85c3613
Nico G <nicolas.giusti@nextsourcia.com>
2023-09-25 17:28:41 +0200

    fix: require/import support in list_modules AQCMS-253

--- services/modules.js
+++ services/modules.js
@@ -760,7 +760,7 @@ const setFrontModuleInTheme = async (pathModule, theme) => {
             type = '';
         }
         const fileNameWithoutModule = file.replace('.js', '').toLowerCase(); // ComponentName.js -> componentname
-        const jsxModuleToImport     = `{jsx: require('./${parsedInfo.info.name}/${file}'), code: 'aq-${fileNameWithoutModule}', type: '${type}'},`;
+        const jsxModuleToImport     = `{jsx: ${moduleComponentType !== '' ? 'import' : 'require'}('./${parsedInfo.info.name}/${file}'), code: 'aq-${fileNameWithoutModule}', type: '${type}'},`;
         const result                = await fs.readFile(pathListModules, 'utf8');
 
         // file don't contain module name
