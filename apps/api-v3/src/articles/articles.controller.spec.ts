caaab5fcf7a73172bc774b603154d5d0e4b0901e
MaelVB <vaillant.beuchot.mael@gmail.com>
2024-12-17 17:44:12 +0100

    feat: Update units tests for article

--- apps/api-v3/src/articles/articles.controller.spec.ts
+++ apps/api-v3/src/articles/articles.controller.spec.ts
@@ -1,20 +1,145 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+
+const articlesServiceMock = {
+  create: jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('ArticlesController', () => {
   let controller: ArticlesController;
+  let service: jest.Mocked<ArticlesService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticlesController],
-      providers: [ArticlesService],
+      providers: [
+        {
+          provide: ArticlesService,
+          useValue: articlesServiceMock,
+        },
+      ],
     }).compile();
 
-    controller = module.get<ArticlesController>(ArticlesController);
+    controller = module.get(ArticlesController);
+    service = module.get(ArticlesService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should create a new article', async () => {
+      const mockedArticle = {
+        _id: new Types.ObjectId(),
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      service.create.mockResolvedValueOnce(mockedArticle);
+
+      const createArticleDto: CreateArticleDto = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      const result = await controller.create(createArticleDto);
+
+      expect(result).toEqual(mockedArticle);
+      expect(service.create).toHaveBeenCalledWith(createArticleDto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of articles', async () => {
+      const mockedArticles = [
+        {
+          _id: new Types.ObjectId(),
+          code: 'Article #1',
+          title: 'Title #1',
+          coverImage: 'Url #1',
+        },
+        {
+          _id: new Types.ObjectId(),
+          code: 'Article #2',
+          title: 'Title #2',
+          coverImage: 'Url #2',
+        },
+        {
+          _id: new Types.ObjectId(),
+          code: 'Article #3',
+          title: 'Title #3',
+          coverImage: 'Url #3',
+        },
+      ];
+      service.findAll.mockResolvedValueOnce(mockedArticles);
+
+      const result = await controller.findAll();
+
+      expect(result).toEqual(mockedArticles);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single article', async () => {
+      const mockedArticle = {
+        _id: new Types.ObjectId(),
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      service.findOne.mockResolvedValueOnce(mockedArticle);
+
+      const id = new Types.ObjectId().toString();
+      const result = await controller.findOne(id);
+
+      expect(result).toEqual(mockedArticle);
+      expect(service.findOne).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a single article', async () => {
+      const mockedArticle = {
+        _id: new Types.ObjectId(),
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      service.update.mockResolvedValueOnce(mockedArticle);
+
+      const id = new Types.ObjectId().toString();
+      const updateArticleDto: CreateArticleDto = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      const result = await controller.update(id, updateArticleDto);
+
+      expect(result).toEqual(mockedArticle);
+      expect(service.update).toHaveBeenCalledWith(id, updateArticleDto);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a single article', async () => {
+      const mockedArticle = {
+        _id: new Types.ObjectId(),
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      service.remove.mockResolvedValueOnce(mockedArticle);
+
+      const id = new Types.ObjectId().toString();
+      const result = await controller.remove(id);
+
+      expect(result).toEqual(mockedArticle);
+      expect(service.remove).toHaveBeenCalledWith(id);
+    });
   });
 });
--- apps/api-v3/src/articles/articles.service.spec.ts
+++ apps/api-v3/src/articles/articles.service.spec.ts
@@ -1,18 +1,150 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model, Types } from 'mongoose';
 import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { Article } from './schemas/article.schema';
+
+const articleModelMock = {
+  create: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  findByIdAndUpdate: jest.fn(),
+  findByIdAndDelete: jest.fn(),
+};
 
 describe('ArticlesService', () => {
   let service: ArticlesService;
+  let model: jest.Mocked<Model<Article>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticlesService],
+      providers: [
+        ArticlesService,
+        {
+          provide: getModelToken('Article'),
+          useValue: articleModelMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<ArticlesService>(ArticlesService);
+    service = module.get(ArticlesService);
+    model = module.get(getModelToken('Article'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should insert a new article', async () => {
+      const mockedArticle: CreateArticleDto = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      model.create.mockResolvedValueOnce(mockedArticle as any);
+
+      const createArticleDto = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      const result = await service.create(createArticleDto);
+
+      expect(result).toEqual(mockedArticle);
+      expect(model.create).toHaveBeenCalledWith(createArticleDto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all articles', async () => {
+      const mockedArticles = [
+        {
+          code: 'Article #1',
+          title: 'Title #1',
+          coverImage: 'Url #1',
+        },
+        {
+          code: 'Article #2',
+          title: 'Title #2',
+          coverImage: 'Url #2',
+        },
+      ];
+      model.find.mockReturnValueOnce({
+        exec: jest.fn().mockResolvedValueOnce(mockedArticles),
+      } as any);
+
+      const result = await service.findAll();
+
+      expect(result).toEqual(mockedArticles);
+      expect(model.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return one article', async () => {
+      const mockedArticle = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      model.findOne.mockReturnValueOnce({
+        exec: jest.fn().mockResolvedValueOnce(mockedArticle),
+      } as any);
+
+      const id = new Types.ObjectId().toString();
+      const result = await service.findOne(id);
+
+      expect(result).toEqual(mockedArticle);
+      expect(model.findOne).toHaveBeenCalledWith({ _id: id });
+    });
+  });
+
+  describe('update', () => {
+    it('should update a article', async () => {
+      const mockedArticle = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      model.findByIdAndUpdate.mockReturnValueOnce({
+        exec: jest.fn().mockResolvedValueOnce(mockedArticle),
+      } as any);
+
+      const id = new Types.ObjectId().toString();
+      const updateArticleDto = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      const result = await service.update(id, updateArticleDto);
+
+      expect(result).toEqual(mockedArticle);
+      expect(model.findByIdAndUpdate).toHaveBeenCalledWith(
+        { _id: id },
+        updateArticleDto,
+        { new: true },
+      );
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a article', async () => {
+      const mockedArticle = {
+        code: 'Article #1',
+        title: 'Title #1',
+        coverImage: 'Url #1',
+      };
+      model.findByIdAndDelete.mockReturnValueOnce({
+        exec: jest.fn().mockResolvedValueOnce(mockedArticle),
+      } as any);
+
+      const id = new Types.ObjectId().toString();
+      const result = await service.remove(id);
+
+      expect(result).toEqual(mockedArticle);
+      expect(model.findByIdAndDelete).toHaveBeenCalledWith({ _id: id });
+    });
   });
 });
